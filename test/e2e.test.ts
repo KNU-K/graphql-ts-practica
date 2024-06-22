4600612505e162ac88a490b03f1c9bd44e0c2e07
KNU-K <knukang334@gmail.com>
2024-06-23 03:47:48 +0900

    [ch3] e2e 테스트 코드 구성 (Under-Fetching 시나리오)

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -10,72 +10,31 @@ describe("✅ RestAPI test", () => {
         app = await createRestApp();
     });
 
-    it("✅✅ 프로필, 특정 게시물 정보, 댓글 정보 요청", async () => {
-        const resProfile = await request(app).get("/api/profile");
-        const resBoard = await request(app).get("/api/board/1");
-        const resComment = await request(app).get("/api/comment").query({ board_id: 1 });
-
-        /** getProfile */
-        expect(resProfile.statusCode).toEqual(200);
-        expect(resProfile.body).toHaveProperty("id");
-        expect(resProfile.body).toHaveProperty("name");
-        expect(resProfile.body).toHaveProperty("email");
-
-        /** getBoard */
-        expect(resBoard.statusCode).toEqual(200);
-        expect(resBoard.body).toHaveProperty("postId");
-        expect(resBoard.body.postId).toEqual(1);
-        expect(resBoard.body).toHaveProperty("userId");
-        expect(resBoard.body).toHaveProperty("title");
-
-        /** getComment */
-        expect(resComment.statusCode).toEqual(200);
-        expect(resComment.body).toHaveProperty("commentId");
-        expect(resComment.body).toHaveProperty("postId");
-        expect(resComment.body.postId).toEqual(1);
-        expect(resComment.body).toHaveProperty("userId");
-        expect(resComment.body).toHaveProperty("content");
-    });
-});
-
-describe("✅ GraphQL test", () => {
-    let app: Application;
-
-    beforeAll(async () => {
-        app = await createGraphApp();
-    });
-
-    it("✅✅ 프로필, 특정 게시물 정보, 댓글 정보 요청", async () => {
-        const query = `query {
-                            userProfile(id: 1) {
-                                id
-                                name
-                                email
-                                age
-                                phone
-                            }
-                            getBoard(id:1){
-                                postId
-                                userId
-                                title
-                                content
-                                timestamp
-                            }
-                            getComment(boardId:1){
-                                commentId
-                                postId
-                                userId
-                                content
-                                timestamp
-                            }
-                        }`;
-        const res = await request(app).post("/graphql").send({
-            query: query,
-        });
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty("data");
-        expect(res.body.data).toHaveProperty("userProfile");
-        expect(res.body.data).toHaveProperty("getBoard");
-        expect(res.body.data).toHaveProperty("getComment");
+    it("✅✅ 프로필, 리뷰, 리뷰 세부정보", async () => {
+        const userRes = await request(app).get("/api/user/1");
+        const userReviewRes = await request(app).get("/api/user/1/review");
+        const reviewRes = await request(app).get("/api/review/1");
+        expect(userRes.status).toBe(200);
+        expect(userRes.body).toHaveProperty("id");
+        expect(userRes.body).toHaveProperty("name");
+        expect(userRes.body).toHaveProperty("email");
+        expect(userRes.body).toHaveProperty("age");
+        expect(userRes.body).toHaveProperty("phone");
+        expect(userReviewRes.status).toBe(200);
+        expect(userReviewRes.body).toHaveProperty("reviewId");
+        expect(userReviewRes.body).toHaveProperty("userId");
+        expect(userReviewRes.body).toHaveProperty("content");
+        expect(userReviewRes.body).toHaveProperty("timestamp");
+        expect(userReviewRes.status).toBe(200);
+        expect(userReviewRes.body).toHaveProperty("reviewId");
+        expect(userReviewRes.body).toHaveProperty("userId");
+        expect(userReviewRes.body).toHaveProperty("content");
+        expect(userReviewRes.body).toHaveProperty("timestamp");
+        expect(reviewRes.status).toBe(200);
+        expect(reviewRes.body).toHaveProperty("reviewId");
+        expect(reviewRes.body).toHaveProperty("timestamp");
+        expect(reviewRes.body).toHaveProperty("userId");
+        expect(reviewRes.body).toHaveProperty("content");
+        expect(reviewRes.body).toHaveProperty("score");
     });
 });
