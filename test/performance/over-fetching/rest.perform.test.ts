07e643af68da50634900f4b8e362a975b6ab5ca7
KNU-K <knukang334@gmail.com>
2024-06-24 23:12:29 +0900

    [ch] 시나리오에 따른 e2e 테스트 및 성능 Test
    Over-fetching 경우
    under-fetching 경우

--- test/performance/over-fetching/rest.perform.test.ts
+++ test/performance/over-fetching/rest.perform.test.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import createRestApp from "../src/restapi/app";
+import createRestApp from "../../../src/restapi/app";
 import { Application } from "express";
 import { performance } from "perf_hooks";
 
@@ -17,20 +17,19 @@ describe("✅ RestAPI Performance Test", () => {
         for (let i = 0; i < numRequests; i++) {
             const startTime = performance.now();
 
-            // 프로필 정보 요청
-            await request(app).get("/api/user/1").expect(200);
-
-            // 사용자 리뷰 목록 요청
-            const userReviewRes = await request(app).get("/api/user/1/review").expect(200);
-            for (const userReview of userReviewRes.body) {
-                await request(app).get(`/api/review/${userReview.reviewId}`).expect(200);
-            }
+            const userRes = await request(app).get("/api/user");
+            expect(userRes.status).toBe(200);
 
             const endTime = performance.now();
             const duration = endTime - startTime;
             totalDuration += duration;
 
-            console.log(`Request ${i + 1} 소요 시간: ${duration} milliseconds`);
+            console.log(
+                `Request ${i + 1} 소요 시간: ${duration} milliseconds, Response Size: ${
+                    userRes.header["content-length"]
+                } bytes`
+            );
+            expect(userRes.status).toBe(200);
         }
 
         const averageDuration = totalDuration / numRequests;
